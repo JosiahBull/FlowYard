3add199436b73e1fae4bf45592e37bdf910e0f80
badtoro2 <josiah.bull7@gmail.com>
2019-12-01 19:57:29 +1300

    New solveLIneCollisions Function
    New function solves for collisions, adds collision point, and adds another line from collision to end point while adjusting existing line to finish at endpoint.
    
    Basically makes junctions work as expected on a 2D plane.

--- 03_Code/index.js
+++ 03_Code/index.js
@@ -53,37 +53,64 @@ function processLineFile(lineFile) {
     return Promise.resolve(rawLineInformation);
 };
 
-function solveLineCollisions(lineInformation) {
-    let outputCollisions = [];
-    let remaining = JSON.parse(JSON.stringify(lineInformation));
-    lineInformation.forEach((line, i) => {
-        //Calculate Linear Equation of First Line
-        let { coord1, coord2 } = line;
-        let m = (coord1[1]-coord2[1])/(coord1[0]-coord2[0]);
-        let b = coord1[0]+m*coord1[1];
-        let minX = Math.min(coord1[0], coord2[0]);
-        let maxX = Math.max(coord2[0], coord2[0]);
-        remaining.forEach((secondLine, x) => {
+function solveLineCollisions(points, existingLines) {
+    function findPointInArray(node) {
+        return points.filter(point => node === point.id)[0];
+    }
+    let lines = JSON.parse(JSON.stringify(existingLines));
+    existingLines.forEach((line, i) => {
+        //Calculate linear equation of first line.
+        let { startNode, endNode } = line;
+        startNode = findPointInArray(startNode);
+        endNode = findPointInArray(endNode);
+        let m = (startNode.y-endNode.y)/(startNode.x-endNode.x);
+        let b = startNode.x+m*startNode.y;
+        let minX = Math.min(startNode.x, endNode.x);
+        let maxX = Math.max(startNode.x, endNode.x);
+
+        existingLines.forEach((secondLine, r) => {
             //Calculate Linear Equation of Second Line
-            let { coord1, coord2 } = secondLine;
-            let q = (coord1[1]-coord2[1])/(coord1[0]-coord2[0]);
-            let c = coord1[0]+m*coord1[1];
+            let { startNode, endNode } = secondLine;
+            startNode = findPointInArray(startNode);
+            endNode = findPointInArray(endNode);
+            let q = (startNode.y-endNode.y)/(startNode.x-endNode.x);
+            let c = startNode.x+m*startNode.y;
             if (m === q) return; //Lines are parallel and will never meet.
             //Check for collision between lines.
             collisionX = (c-b)/(m-q);
             //If the collision exists on the extent of the lines, then add it.
-            if (collisionX > minX && collisionX < maxX) {
-                outputCollisions.push([
-                    collisionX,
-                    m*collisionX+b //Calculate Y value.
-                ]);
-
-                
-
+            if (collisionX > minX && collisionX < maxX) { 
+                //Create new point and add to pointArray.
+                newPoint = {
+                    id: getPointId(),
+                    x: collisionX,
+                    y: m*collisionX+b,
+                    z: null
+                };
+                points.push(newPoint);
+                //Add two new lines because of new breakpoints.
+                lines.push({
+                    id: getLineId(),
+                    startNode: newPoint.id,
+                    endNode: lines[i].endNode,
+                    length: calcLength(newPoint, findPointInArray(lines[i].endNode))
+                });
+                lines.push({
+                    id: getLineId(),
+                    startNode: newPoint.id,
+                    endNode: lines[r].endNode,
+                    length: calcLength(newPoint, findPointInArray(lines[r].endNode))
+                });
+                //Edit the two existing lines to be shorter and finish at the new point.
+                lines[i].endNode = newPoint.id
+                lines[r].endNode = newPoint.id
             };
         });
     });
-    return outputCollisions;
+    return {
+        points: points,
+        lines: lines
+    }
 };
 
 function saveOuput(junctions, pipes, coordinates, vertices){
@@ -111,8 +138,6 @@ function saveOuput(junctions, pipes, coordinates, vertices){
     fs.writeFileSync('forProcessing.inp', saveFile, 'utf-8');
 };
 
-console.log('BlueBarn Parser Tool Started.');
-
 /* EXPECTED SCHEMA
     Junction : id, elev, demand (0), pattern (empty).
     Pipe : id, node1, node2, length, diameter, roughness (100), minorloss (0), status (open)
@@ -127,7 +152,20 @@ function containsObject(obj, list) {
     }
     return [true, null];
 };
+let pointCounter = 0;
+function getPointId() {
+    return pointCounter++;
+}
+let lineCounter = 0;
+function getLineId() {
+    return lineCounter++
+}
 
+function calcLength(point1, point2) {
+    return Math.sqrt(Math.pow(Math.abs(point1.x - point2.x), 2) + Math.pow(Math.abs(point1.y - point2.y), 2))
+}
+
+console.log('BlueBarn Parser Tool Started.');
 Promise.all([
     getShapeFile(pathToLineFile).then(x => processLineFile(x)),
     getCSVFile(pathToPointFile)
@@ -137,72 +175,42 @@ Promise.all([
         rawPointInformation: x[1]
     }
 }).then(shapeInformation => {
-    let pointCounter = 0;
-    let lineCounter = 0;
     let points = [];
     let lines = [];
-    solveLineCollisions(shapeInformation.rawLineInformation).forEach(collision => {
-        points.push({
-            id: pointCounter,
-            x: collision[0],
-            y: collision[1],
-            z: null
-        })
-        pointCounter++;
-    });
     shapeInformation.rawLineInformation.forEach(line => {
         let point = containsObject(line.coord1, points);
         if (point[0]) {
-            points.push({
-                id: pointCounter,
-                x: line.coord1[0],
-                y: line.coord1[1],
-                z: null
-            })
             point = {
-                id: pointCounter,
+                id: getPointId(),
                 x: line.coord1[0],
                 y: line.coord1[1],
                 z: null
             }
-            pointCounter++;
+            points.push(point)
         } else {
             point = point[1];
         }
         let point2 = containsObject(line.coord2, points);
         if (point2[0]) {
-            points.push({
-                id: pointCounter,
-                x: line.coord2[0],
-                y: line.coord2[1],
-                z: null
-            })
             point2 = {
-                id: pointCounter,
+                id: getPointId(),
                 x: line.coord2[0],
                 y: line.coord2[1],
                 z: null
             }
-            pointCounter++;
+            points.push(point2);
         } else {
             point2 = point2[1];
         }
         lines.push({
-            id: lineCounter,
+            id: getLineId(),
             startNode: point.id,
             endNode: point2.id,
-            length: Math.sqrt(Math.pow(Math.abs(point.x - point2.x), 2) + Math.pow(Math.abs(point.y - point2.y), 2))
+            length: calcLength(point, point2)
         })
-        lineCounter++;
     })
 
-    console.log(({
-        points: points,
-        lines: lines
-    }))
-
-    // console.log(shapeInformation)
-
+    return solveLineCollisions(points, lines);
 }).catch(err => {
     console.log('\nAn error occured!\n');
     console.error(err.stack);
