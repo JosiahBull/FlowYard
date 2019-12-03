87dbdd84239883701b72ce95967c11dab534ad12
badtoro2 <josiah.bull7@gmail.com>
2019-12-03 21:35:01 +1300

    Linecollisions Function Added
    and all other required functions

--- 03_Code/index.js
+++ 03_Code/index.js
@@ -8,27 +8,151 @@ const toleranceValue = 0.01;
 const Counter = (function() {
     let counterValue = 0;
     return {
-        increment : function() {
+        increment: function() {
             return counterValue++;
         },
-        getCurrent : function() {
+        decrement: function() {
+            return counterValue--;
+        },
+        getCurrent: function() {
             return counterValue;
         }
     }
 });
 const pipeId = new Counter;
 const nodeId = new Counter;
-
+//Helper Functions
 function round(value) {
     return Number(value.toFixed(4));
-}
-let get = {
-    length: function(point1, point2) {
-        return round(Math.sqrt(Math.pow(Math.abs(point1.x - point2.x), 2) + Math.pow(Math.abs(point1.y - point2.y), 2)));
+};
+function changeState(input) {
+    let output;
+    if (Array.isArray(input)) {
+        output = {};
+        input.forEach(subObj => {
+            let subObjId = subObj.id;
+            delete subObj.id;
+            output[subObjId] = subObj;
+        })
+    } else {
+        output = [];
+        for (subObj in input) {
+            output.push({
+                ...input[subObj],
+                id: Number(subObj)
+            })
+        }
     }
-}
+    return output;
+};
+function duplicatePoint(checkPoint, points) {
+    let filteredPoints = points.filter( point => point.x === checkPoint.x && point.y === checkPoint.y);
+    return (filteredPoints.length > 0) ? {value: true, point: filteredPoints[0]} : {value: false, point: checkPoint};
+};
+function clean(input) {
+    return JSON.parse(JSON.stringify(input));
+};
+
+//Main Functions
+
+let get = (function() {
+    function resolveLineCollisions(lines, points, collisions) {
+        let collisionRegistry = {};
+        collisions.forEach(collision => {
+            let { line, secondLine, xCollision } = collision;
+            let newPoint, newLine, newSecondLine;
+            if (collisionRegistry[line.id] === secondLine.id) return; //These two lines have already collided.
+            collisionRegistry[secondLine.id] = line.id; //Add this Id to the collision registry to prevent these two lines colliding again.
+            newPoint = duplicatePoint({
+                x: xCollision,
+                y: round(line.equation.grad * xCollision + line.equation.yInt)
+            }, changeState(points));
+            if (this.length(newPoint.point, points[line.endNode]) < toleranceValue || this.length(newPoint.point, points[secondLine.endNode]) < toleranceValue) return; //If your length is 0 so is your worth.
+            if (!newPoint.value) {
+                newPoint = newPoint.point;
+                points[nodeId.getCurrent()] = clean(newPoint);
+                newPoint.id = nodeId.increment();
+            };
+            newLine = {
+                startNode: newPoint.id,
+                endNode: line.endNode,
+                length: this.length(newPoint, points[line.endNode]),
+                lineId: line.lineId
+            };
+            newSecondLine = {
+                startNode: newPoint.id,
+                endNode: secondLine.endNode,
+                length: this.length(newPoint, points[secondLine.endNode]),
+                lineId: secondLine.lineId
+            };
+            //Add two new lines because of the breakpoint.
+            lines[pipeId.increment()] = newLine;
+            lines[pipeId.increment()] = newSecondLine;
+            //Edit the two existing lines to end at the new collision point.
+            lines[line.id].endNode = newPoint.id;
+            lines[line.id].length = this.length(points[lines[line.id].startNode], points[lines[line.id].endNode]);
+            lines[secondLine.id].endNode = newPoint.id;
+            lines[secondLine.id].length = this.length(points[lines[secondLine.id].startNode], points[lines[secondLine.id].endNode]);
+        });
+        return {
+            lines: lines,
+            points: points
+        }
+    };
+    function resolvePointCollisions(lines, points, collisions) {
+    };
+    function lineEquation(startNode, endNode) {
+        let isVertical = (startNode.x - endNode.x === 0) ? true : false; //Checking if this is a vertical line.
+        let grad = (isVertical) ? startNode.x : (startNode.y - endNode.y)/(startNode.x - endNode.x);
+        let yInt = (isVertical) ? null : startNode.y - grad * startNode.x;
+        return {
+            isVertical: isVertical,
+            grad: grad,
+            yInt: yInt
+        }
+    };
+    function lineExtents(line1StartNode, line1EndNode, line2StartNode, line2EndNode) {
+        return {
+            min: Math.max(Math.min(line1StartNode.x, line1EndNode.x), Math.min(line2StartNode.x, line2EndNode.x)),
+            max: Math.min(Math.max(line1StartNode.x, line1EndNode.x), Math.max(line2StartNode.x, line2EndNode.x))
+        }
+    };
+    return {
+        lineCollisions: function(lines, points) {
+            let collisionCounter = new Counter;
+            let collisions = [];
+            changeState(lines).forEach((line, i) => {
+                line.equation = lineEquation(points[line.startNode], points[line.endNode])
+                changeState(lines).forEach((secondLine, r) => {
+                    secondLine.equation = lineEquation(points[secondLine.startNode], points[secondLine.endNode]);
+                    if (line.equation.grad === secondLine.equation.grad || (line.equation.isVertical && secondLine.equation.isVertical)) return; //Lines have the same gradient and will never meet (paralell).
+                    let xCollision = round((line.equation.yInt-secondLine.equation.yInt)/(secondLine.equation.grad-line.equation.grad));
+                    if (line.equation.isVertical) xCollision = line.equation.grad;
+                    if (secondLine.equation.isVertical) xCollision = secondLine.equation.grad;
+                    let xOverlap = lineExtents(points[line.startNode], points[line.endNode], points[secondLine.startNode], points[secondLine.endNode]);
+                    if (Math.abs(xCollision - xOverlap.min) >= toleranceValue && Math.abs(xCollision - xOverlap.max) >= toleranceValue) {
+                        collisions.push({
+                            id: collisionCounter.increment(),
+                            line: line,
+                            secondLine: secondLine,
+                            xCollision: xCollision
+                        });
+                    }
+                })
+            });
+            return resolveLineCollisions.call(this, lines, points, collisions);
+        },
+        pointCollisions: function(lines, points) {
+
+        },
+        length: function(point1, point2) {
+            return round(Math.sqrt(Math.pow(Math.abs(point1.x - point2.x), 2) + Math.pow(Math.abs(point1.y - point2.y), 2)));
+        }
+    }
+})();
+
 let load = {
-    shapeFile : async function(shapeFilePath) {
+    shapeFile: async function(shapeFilePath) {
         let output = [];
         let points = {};
         let lines = {};
@@ -74,7 +198,7 @@ let load = {
             lines: lines
         };
     },
-    pointFile : async function(pointFilePath) {
+    pointFile: async function(pointFilePath) {
         return csv({
             noheader: true,
             trim: true,
@@ -91,13 +215,6 @@ let load = {
             });
     }
 }
-
-load.shapeFile(path.join(__dirname, 'RawInput/shapey.shp')).then(result => {
-    console.log(result)
-}).catch(err => {
-    console.log('An error ocurred!');
-    console.log(err)
-})
 
 
 // function loadPipeNetworks(networkArray) {
